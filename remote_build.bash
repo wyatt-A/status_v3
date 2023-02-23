938882437a46a2281fb9b6d63b388c725332105c
James Cook <jamesjcook@users.noreply.github.com>
2023-02-23 14:24:58 -0500

    Fixed remote build

--- remote_build.bash
+++ remote_build.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for hst in delos civmcluster1 vidconfmac piper; do
-    ssh $hst 'bash -c "cd $WORKSTATION_CODE/archive/pipe_status;./build.sh"' &
+    ssh $hst 'bash -c "cd $WORKSTATION_CODE/archive/pipe_status;git stash; git pull; git stash pop; ./build.bash"' &
 done
 wait;
